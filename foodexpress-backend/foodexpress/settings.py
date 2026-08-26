66a41e05164bfbd51afaa7fb4af2556019b237d4
RohindhShan <rohindhbackup@gmail.com>
2026-08-26 23:47:02 +0530

    feat: register restaurant models in admin

--- foodexpress-backend/foodexpress/settings.py
+++ foodexpress-backend/foodexpress/settings.py
@@ -114,7 +114,7 @@ WSGI_APPLICATION = "foodexpress.wsgi.application"
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600
     )
 }
